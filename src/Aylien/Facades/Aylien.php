31813e936db92ad301263309a0fe5389f7a64738
Michael Hopkins <michael@appitventures.com>
2015-02-19 22:15:56 +0000

    first commit

---
+++ src/Aylien/Facades/Aylien.php
@@ -0,0 +1,14 @@
+<?php namespace Hopkins\LaravelAylienWrapper\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Aylien extends Facade {
+
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor() { return 'hopkins.aylien'; }
+
+}
---
+++ src/Aylien/config/config.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+
+    /*
+    |-------------------------------------------------------------
+    | App Id
+    |-------------------------------------------------------------
+    |
+    | The Id of your Aylien application.
+    | https://developer.aylien.com/admin/applications
+    |
+    */
+
+    'app_id' => 'XXXXXXXX',
+
+    /*
+    |-------------------------------------------------------------
+    | App Key
+    |-------------------------------------------------------------
+    |
+    | One of the application keys for the above Aylien app
+    | https://developer.aylien.com/admin/applications/XXXXXXXXXXXXXX
+    |
+    */
+    'app_key' => 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+];
